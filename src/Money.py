14d68a3a2d3e27b9b4623b4c7fc4e3f838ed169e
Tokuma Suzuki <tokuma.suzuki09@gmail.com>
2021-05-11 21:22:32 +0900

    simplify methods

--- src/Money.py
+++ src/Money.py
@@ -3,7 +3,4 @@ class Money():
         self._amount = amount
 
     def __eq__(self, money):
-        return self._amount == money._amount
-
-    def equals(self, money):
-        return self._amount == money._amount
+        return (self._amount == money._amount) & (type(self) == type(money))
