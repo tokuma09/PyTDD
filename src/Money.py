4c95626ea6e71c73bf181842fafe2c8cb4f2e921
Tokuma Suzuki <tokuma.suzuki09@gmail.com>
2021-05-11 22:35:35 +0900

    add abstract and static method

--- src/Money.py
+++ src/Money.py
@@ -1,6 +1,25 @@
-class Money():
+from abc import ABCMeta, abstractmethod
+
+
+class Money(metaclass=ABCMeta):
     def __init__(self, amount):
         self._amount = amount
 
+    @abstractmethod
+    def times(self, multiplier):
+        pass
+
     def __eq__(self, money):
         return (self._amount == money._amount) & (type(self) == type(money))
+
+    @staticmethod
+    def dollar(amount):
+        from src.Dollar import Dollar
+
+        return Dollar(amount)
+
+    @staticmethod
+    def franc(amount):
+        from src.Franc import Franc
+
+        return Franc(amount)
