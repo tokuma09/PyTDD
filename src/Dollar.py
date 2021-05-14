4f206c63daa511ccbe04881b83fc0dcebe905c21
Tokuma Suzuki <tokuma.suzuki09@gmail.com>
2021-05-14 21:33:52 +0900

    generalize times

--- src/Dollar.py
+++ src/Dollar.py
@@ -4,6 +4,3 @@ from src.Money import Money
 class Dollar(Money):
     def __init__(self, amount, currency):
         super().__init__(amount, currency)
-
-    def times(self, multiplier):
-        return Money.dollar(self._amount * multiplier)
--- src/Franc.py
+++ src/Franc.py
@@ -4,6 +4,3 @@ from src.Money import Money
 class Franc(Money):
     def __init__(self, amount, currency):
         super().__init__(amount, currency)
-
-    def times(self, multiplier):
-        return Money.franc(self._amount * multiplier)
